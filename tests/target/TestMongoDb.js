65b691639f87f788f3b58ff7370c3c0262a00934
Charles Xiong <ex1t@CharlesdeMBP.lan>
2019-11-07 08:35:27 +0800

    fixed

--- tests/target/TestMongoDb.js
+++ tests/target/TestMongoDb.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var path = require('path')
-var utils = require(path.resolve(__dirname,"utils.js"))
+var utils = require(path.resolve(__dirname,"Utils.js"))
 var traceCmp = require(path.resolve(__dirname,"../../../taintable/utils/traceCmp.js"))
 
 const url = 'mongodb://localhost:27017';
