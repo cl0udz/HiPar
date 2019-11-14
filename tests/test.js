4272a4d9c5ec5a7e9f3209bb2ec6528a2008a46c
Charles Xiong <22375724+S-Ex1t@users.noreply.github.com>
2019-11-15 01:20:02 +0800

    added TestJoi

--- tests/test.js
+++
@@ -1,4 +0,0 @@
-var tmp = require('tmp');
-var path = require('path')
-var projTmpDir = tmp.dirSync({"dir":path.resolve(__dirname,"target_tmp")});
-console.log(path.resolve(__dirname,"target_tmp"),projTmpDir.name)
