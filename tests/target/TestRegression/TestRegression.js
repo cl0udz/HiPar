1df1ed3b4e4f929bd8d6dfc9f03b67332493f5c8
Charles Xiong <22375724+S-Ex1t@users.noreply.github.com>
2019-11-15 22:14:39 +0800

    babel regression

--- tests/target/TestRegression/TestRegression.js
+++ tests/target/TestRegression/TestRegression.js
@@ -1,9 +1,12 @@
+"use strict";
+
 var do_test = require("regre_tester");
+
 var path = require('path');
+
 var utils = require(path.resolve(__dirname, "../Utils.js"));
 
 var user_input = {
   'username': 'a'
 };
-
 utils.whatWeDoThisTime(do_test, user_input, __dirname);
