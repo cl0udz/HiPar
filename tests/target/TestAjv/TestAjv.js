0b007605d053d2a291a09e99e96d91d83024bff5
Charles Xiong <22375724+S-Ex1t@users.noreply.github.com>
2019-11-20 09:58:13 +0800

    added schema inspector

---
+++ tests/target/TestAjv/TestAjv.js
@@ -0,0 +1,18 @@
+var ajv = new Ajv({$data: true});
+
+var schema = {
+  "properties": {
+    "smaller": {
+      "type": "number",
+      "maximum": { "$data": "1/larger" }
+    },
+    "larger": { "type": "number" }
+  }
+};
+
+var validData = {
+  smaller: 5,
+  larger: 7
+};
+
+ajv.validate(schema, validData); // true
---
+++ tests/target/TestSchema-inspector/TestSchema-inspector.js
@@ -0,0 +1,61 @@
+var inspector = require('schema-inspector');
+
+// Data that we want to sanitize and validate
+var data = {
+	firstname: 'sterling  ',
+	lastname: '  archer',
+	jobs: 'Special agent, cocaine Dealer',
+	email: 'NEVER!',
+};
+
+
+function test(data){
+    // Sanitization Schema
+    var sanitization = {
+        type: 'object',
+        properties: {
+            firstname: { type: 'string', rules: ['trim', 'title'] },
+            lastname: { type: 'string', rules: ['trim', 'title'] },
+            jobs: {
+                type: 'array',
+                splitWith: ',',
+                items: { type: 'string', rules: ['trim', 'title'] }
+            },
+            email: { type: 'string', rules: ['trim', 'lower'] }
+        }
+    };
+    // Let's update the data
+    inspector.sanitize(sanitization, data);
+    /*
+    data is now:
+    {
+        firstname: 'Sterling',
+        lastname: 'Archer',
+        jobs: ['Special Agent', 'Cocaine Dealer'],
+        email: 'never!'
+    }
+    */
+
+    // Validation schema
+    var validation = {
+        type: 'object',
+        properties: {
+            firstname: { type: 'string', minLength: 1 },
+            lastname: { type: 'string', minLength: 1 },
+            jobs: {
+                type: 'array',
+                items: { type: 'string', minLength: 1 }
+            },
+            email: { type: 'string', pattern: 'email' }
+        }
+    };
+    var result = inspector.validate(validation, data);
+    if (!result.valid)
+        console.log(result.format());
+    /*
+        Property @.email: must match [email], but is equal to "never!"
+    */
+}
+
+var utils = require('../TestcaseUtils');
+utils.whatWeDoThisTime(test,data,__dirname);
