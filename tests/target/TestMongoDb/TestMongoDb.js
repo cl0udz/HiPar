73183f0a21060023955e9b996eb9d03b2f9a9725
Charles Xiong <22375724+S-Ex1t@users.noreply.github.com>
2019-11-30 14:58:37 +0800

    fixed bug in Testmongodb

--- tests/target/TestMongoDb/TestMongoDb.js
+++ tests/target/TestMongoDb/TestMongoDb.js
@@ -46,18 +46,6 @@ const findOneDocuments = function (db, query, callback) {
   });
 }
 
-const findAllDocuments = function (db, callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function (err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
-  });
-}
-
 
 // connect and  find result of query 
 function test(query) {
