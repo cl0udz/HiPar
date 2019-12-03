3f750bff94d819285d45a26c5a0ea6fc77176a35
Jianwei <hjw@tamu.edu>
2019-12-03 10:34:33 -0600

    rever json-strictify

---
+++ tests/target/TestJSON-strictify/TestJSON_strictify.js
@@ -0,0 +1,19 @@
+var JSON = require('json-strictify').JSON;
+
+someObject = {
+    code: 42,
+    items: [{
+        id: 1,
+        name: 'foo'
+    }, {
+        id: 2,
+        name: 'bar'
+    }]
+};
+
+function test(input){
+	JSON.stringify(input);
+}
+
+var util = require('../TestUtils.js');
+utils.entry(test, someObject, __dirname);
