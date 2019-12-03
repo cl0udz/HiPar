8589a4377df82c7ace434fe2ac61e2c031c69bef
jianwei <jevon@tamu.edu>
2019-12-03 14:42:48 -0800

    babel ow

--- tests/target/TestOW/TestOW.js
+++ tests/target/TestOW/TestOW.js
@@ -1,1 +1,18 @@
 "use strict";
+
+var _ow = _interopRequireDefault(require("ow"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var unicorn = {
+  rainbow: '🌈',
+  stars: {
+    value: '🌟'
+  }
+};
+(0, _ow["default"])(unicorn, _ow["default"].object.exactShape({
+  rainbow: _ow["default"].string,
+  stars: {
+    value: _ow["default"].number
+  }
+}));
