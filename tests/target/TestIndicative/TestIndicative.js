172b496b22128c32a06c110afa4f547b8347ff22
jianwei <jevon@tamu.edu>
2019-12-02 21:22:26 -0800

    babel validator js

--- tests/target/TestIndicative/TestIndicative.js
+++ tests/target/TestIndicative/TestIndicative.js
@@ -1,17 +1,16 @@
-//require { sanitize  } from 'indicative/sanitizer'
+"use strict";
 
+//require { sanitize  } from 'indicative/sanitizer'
 var santitize = require("indicative").sanitizer;
 
-const rules = {
-      username: 'trim',
-      email: 'normalize_email',
-}
-
-const data = {
-      username: '  foo',
-      email: 'john+doe@gmail.com'  
+var rules = {
+  username: 'trim',
+  email: 'normalize_email'
 };
+var data = {
+  username: '  foo',
+  email: 'john+doe@gmail.com'
+}; // mutates the original data object
 
-// mutates the original data object
 sanitize(data, rules);
 console.log(data);
