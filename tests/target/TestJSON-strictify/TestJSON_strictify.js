07f32a3f288de83c10380254dacef06c50a8001c
jianwei <jevon@tamu.edu>
2019-12-03 09:05:21 -0800

    babel json-strictify

--- tests/target/TestJSON-strictify/TestJSON_strictify.js
+++ tests/target/TestJSON-strictify/TestJSON_strictify.js
@@ -1,19 +1,22 @@
+"use strict";
+
 var JSON = require('json-strictify').JSON;
 
 someObject = {
-    code: 42,
-    items: [{
-        id: 1,
-        name: 'foo'
-    }, {
-        id: 2,
-        name: 'bar'
-    }]
+  code: 42,
+  items: [{
+    id: 1,
+    name: 'foo'
+  }, {
+    id: 2,
+    name: 'bar'
+  }]
 };
 
-function test(input){
-	JSON.stringify(input);
+function test(input) {
+  JSON.stringify(input);
 }
 
 var util = require('../TestUtils.js');
+
 utils.entry(test, someObject, __dirname);
