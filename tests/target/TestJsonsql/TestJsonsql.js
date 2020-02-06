faa8e4367e47bfcad3c05724c3dafc3ec9f2a152
wupco <wupco1996@hotmail.com>
2020-02-06 15:50:13 -0800

    add jsonsql vul

--- tests/target/TestJsonsql/TestJsonsql.js
+++ tests/target/TestJsonsql/TestJsonsql.js
@@ -1,78 +1,74 @@
-"use strict";
-
 var jsonSql = require('json-sql')();
 
 function testselect(query) {
-  var sql = jsonSql.build({
-    type: 'select',
-    table: 'users',
-    fields: ['name', 'age'],
-    condition: query
-  });
+    var sql = jsonSql.build({
+        type: 'select',
+        table: 'users',
+        fields: ['name', 'age'],
+        condition: query
+    });
+    console.log(sql);
 }
 
-function testinsert(query) {
-  var sql = jsonSql.build({
-    type: 'insert',
-    table: 'users',
-    values: query
-  });
+function testinsert(query){
+    var sql = jsonSql.build({
+        type: 'insert',
+        table: 'users',
+        values: query
+    });
 }
 
-function testupdate1(query) {
-  var sql = jsonSql.build({
-    type: 'update',
-    table: 'users',
-    condition: query,
-    modifier: {
-      role: 'admin',
-      age: 33
-    }
-  });
+function testupdate1(query){
+    var sql = jsonSql.build({
+        type: 'update',
+        table: 'users',
+        condition: query,
+        modifier: {
+            role: 'admin',
+            age: 33
+        }
+    });
 }
 
-function testupdate2(query) {
-  var sql = jsonSql.build({
-    type: 'update',
-    table: 'users',
-    condition: {
-      id: 5
-    },
-    modifier: query
-  });
+function testupdate2(query){
+    var sql = jsonSql.build({
+        type: 'update',
+        table: 'users',
+        condition: {
+            id: 5
+        },
+        modifier: query
+    });
 }
 
-function testdelete(query) {
-  var sql = jsonSql.build({
-    type: 'remove',
-    table: 'users',
-    condition: query
-  });
+function testdelete(query){
+    var sql = jsonSql.build({
+        type: 'remove',
+        table: 'users',
+        condition: query
+    });
 }
 
 var utils = require('../TestcaseUtils.js');
 
-function main() {
-  utils.entry(testdelete, {
-    id: 5
-  });
-  utils.entry(testinsert, {
-    name: 'John',
-    lastname: 'Snow',
-    age: 24,
-    gender: 'male'
-  });
-  utils.entry(testupdate1, {
-    id: 5
-  });
-  utils.entry(testupdate2, {
-    role: 'admin',
-    age: 33
-  });
-  utils.entry(testselect, {
-    name: 'Max',
-    id: 6
-  });
-}
+function main(){
+    utils.entry(testdelete, {
+        id: 5
+    });
+    utils.entry(testinsert,{
+        name: 'John',
+        lastname: 'Snow',
+        age: 24,
+        gender: 'male'
+    } );
+    utils.entry(testupdate1,{
+        id: 5
+    });
+    utils.entry(testupdate2,{
+        role: 'admin',
+        age: 33
+    });
+    utils.entry(testselect,{name: 'Max', id: 5});
 
+}
 main();
---
+++ tests/target/TestJsonsql/vul.js
@@ -0,0 +1,18 @@
+var jsonSql = require('json-sql')();
+
+function testselect(query) {
+    var sql = jsonSql.build({
+        type: 'select',
+        table: 'users',
+        fields: ['name', 'age'],
+        condition: query
+    });
+    console.log(sql);
+}
+
+console.log("[*] result of hipar : cast\n");
+testselect({"name":"wupco","id":{"cast":"aaa'\"bbb"}});
+console.log("\n[*] result of hipar : alias\n");
+testselect({"name":"wupco","id":{"alias":"aaa'\"bbb"}});
+
+
